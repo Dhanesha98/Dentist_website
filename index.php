55f6da82f76ba7f179c4627aa2ea8d7cac4a5316
Dhanesha <73052913+Dhanesha98@users.noreply.github.com>
2023-10-15 15:36:05 +0530

    UI/UX

--- index.php
+++ index.php
@@ -27,7 +27,8 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Responsive Dentist Website </title>
+   <title>Dentist Website </title>
+   
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
